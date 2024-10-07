885a84957347f543bfa29e3d28c692182a3c87b7
Santiago~ <santiago-v@udenar.edu.co>
2024-10-07 00:07:21 -0500

    Pagina Mascotas

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,14 +1,14 @@
-import express from "express";
-import { routerMascotas } from "./rutas/mascotasRouter.js";
-import { routerAdoptantes } from "./rutas/adoptantesRouter.js"; // Asegúrate de que la ruta sea correcta
-import { routerSolicitud } from "./rutas/solicitudRouter.js";
+import express from 'express';
+import cors from 'cors';
+import { routerMascotas } from './rutas/mascotasRouter.js';
+import { routerAdoptantes } from './rutas/adoptantesRouter.js';
+import { routerSolicitud } from './rutas/solicitudRouter.js';
+import { db } from './database/conexion.js';
 
-import { db } from "./database/conexion.js";
-
-// Crear instancia de express
 const app = express();
+const PORT = 4000;
 
-// Middleware para parsear el cuerpo de las solicitudes JSON
+app.use(cors());
 app.use(express.json());
 
 // Verificar conexión a la base de datos
@@ -20,34 +20,26 @@ db.authenticate()
         console.log(`Conexión a Base de datos incorrecta: ${err}`);
     });
 
-// Sincronizar la base de datos (forzar recreación de tablas)
-db.sync({ force: true }) 
-.then(() => {
-    // Abrir servicio e iniciar el servidor 
-    app.listen(PORT, () => {
-        console.log(`Servidor inicializado en el puerto ${PORT}`);
+db.sync({ force: false })
+    .then(() => {
+        console.log('Sincronización con la base de datos correcta');
+    })
+    .catch(err => {
+        console.log(`Error al sincronizar base de datos: ${err}`);
     });
-})
-.catch(err => {
-    console.log(`Error al sincronizar base de datos: ${err}`);
-});
 
-// Definir rutas
 app.get('/', (req, res) => {
     res.send('Hola Sitio Principal');
 });
 
-// Llamar rutas de mascotas
 app.use("/mascotas", routerMascotas);
-
 app.use("/adoptantes", routerAdoptantes);
-
 app.use("/solicitudes", routerSolicitud);
 
-// Puerto del servidor
-const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor inicializado en el puerto ${PORT}`);
+});
 
-// Middleware de manejo de errores
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Algo salió mal!');
